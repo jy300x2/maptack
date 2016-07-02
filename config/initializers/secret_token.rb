e9b7f5027c41df40f8831f53254d377fac9dad86
Jon Yu <jy300x2@gmail.com>
2016-07-02 03:02:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maptack::Application.config.secret_key_base = 'f5a9ba0a8665987b017816d5e212238dee8ed8382480e4d93b51d293aad933de834cbe5b8be8f6354d2c722f62d38bae80cba2d2c0d816c518612e17f2dad487'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Maptack::Application.config.session_store :cookie_store, key: '_maptack_session'
