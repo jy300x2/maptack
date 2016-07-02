b8a530f9dc2843435bb75bc3380c91a82f0d3a5f
Jon Yu <jy300x2@gmail.com>
2016-07-02 12:00:02 +0000

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Maptack::Application.configure do
+  config.action_mailer.default_url_options = { host: 'maptack-geo.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
