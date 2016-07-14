86a74ab4a4b19caa872fee9c93c3fa65649f8c59
Jon Yu <jy300x2@gmail.com>
2016-07-14 03:57:15 +0000

    Added geocoder

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,4 +2,6 @@ class Map < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
   validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode
 end
